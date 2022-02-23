c888f574e952a4eab56674e1489a8bb42e66936b
MajoS7 <85207019+MajoS7@users.noreply.github.com>
2022-02-23 00:56:35 -0500

    Intento de hacer menu desplegable

--- src/Components/UI/Nav/Nav.jsx
+++ src/Components/UI/Nav/Nav.jsx
@@ -2,7 +2,26 @@ import React from 'react'
 import { GiFlowerHat } from "react-icons/gi";
 import './Nav.css';
 
+// const buttomsnav =()=>{
+//     const prueba = document.querySelector('#prueba');
+//     const registerButtom = document.createElement('div');
+//     const logButtom = document.createElement('div');
+//     const allUsersButtom = document.createElement('div');
+//     const icono =document.createElement('GiFlowerHat');
 
+//     registerButtom.classList.add('navbuttom');
+//     logButtom.classList.add('navbuttom');
+//     allUsersButtom.classList.add('navbuttom');
+
+//     registerButtom.appendChild(icono);
+//     logButtom.appendChild(icono);
+//     allUsersButtom.appendChild(icono);
+
+//     prueba.appendChild(registerButtom);
+//     prueba.appendChild(logButtom);
+//     prueba.appendChild(allUsersButtom);
+
+// }
 
 export const Nav = () => {
   return (
